bc8491c5d0f457387b949351d7590da8637d04ce
SK <sirajulhaq363@gmail.com>
2021-07-02 11:53:34 +0500

    commit

--- app/Http/Controllers/ApplicationsController.php
+++ app/Http/Controllers/ApplicationsController.php
@@ -10,15 +10,18 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use App\Notifications\LeaveApplied;
+use App\User;
 
-class ApplicationsController extends Controller {
+class ApplicationsController extends Controller
+{
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->middleware('auth');
     }
 
@@ -27,7 +30,8 @@ class ApplicationsController extends Controller {
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index() {
+    public function index()
+    {
         if (Auth::user()->hasPermission('view_application')) {
             if (Auth::user()->hasRole('team_lead')) {
                 $leaves = Application::where('team_lead', Auth::user()->id)->latest()->get();
@@ -40,78 +44,80 @@ class ApplicationsController extends Controller {
         }
     }
 
-    public function pending() {
+    public function pending()
+    {
         if (Auth::user()->hasPermission('view_application')) {
             if (Auth::user()->hasRole('team_lead')) {
                 $pending_leaves = Application::where('status', 2)
-                        ->where('team_lead', Auth ::user()->id)
-                        ->latest()
-                        ->get();
+                    ->where('team_lead', Auth::user()->id)
+                    ->latest()
+                    ->get();
             } else {
                 $pending_leaves = Application::where('status', 2)
-                        ->latest()
-                        ->get();
+                    ->latest()
+                    ->get();
             }
             return view('applications.pending')->with('data', $pending_leaves);
         } else {
             return redirect()->route(
-                            'access-denied');
+                'access-denied'
+            );
         }
     }
 
-    public function rejected() {
+    public function rejected()
+    {
         if (Auth::user()->hasPermission('view_application')) {
             if (Auth::user()->hasRole('team_lead')) {
                 $leaves = Application::where('status', 0)
-                        ->where('team_lead', Auth ::user()->id)
-                        ->latest()
-                        ->get();
+                    ->where('team_lead', Auth::user()->id)
+                    ->latest()
+                    ->get();
             } else {
                 $leaves = Application::where('status', 0)
-                        ->latest()
-                        ->get();
+                    ->latest()
+                    ->get();
             }
-            return view('applications.rejected')->with
-                            ('data', $leaves);
+            return view('applications.rejected')->with('data', $leaves);
         } else {
             return redirect()->route('access-denied');
         }
     }
 
-    public function accepted() {
+    public function accepted()
+    {
         if (Auth::user()->hasPermission('view_application')) {
             if (Auth::user()->hasRole('team_lead')) {
                 $leaves = Application::where('status', 1)
-                        ->where('team_lead', Auth ::user()->id)
-                        ->latest()
-                        ->get();
+                    ->where('team_lead', Auth::user()->id)
+                    ->latest()
+                    ->get();
             } else {
                 $leaves = Application::where('status', 1)
-                        ->latest()
-                        ->get();
+                    ->latest()
+                    ->get();
             }
-            return view('applications.accepted')->with
-                            ('data', $leaves);
+            return view('applications.accepted')->with('data', $leaves);
         } else {
             return redirect()->route('access-denied');
         }
     }
 
-    public function view($id) {
+    public function view($id)
+    {
         if (Auth::user()->hasPermission('view_application')) {
             $leave = Application::find($id);
-            $count_accept = Application::where('user_id', $leave->user_id)
-                    ->where('status', 1)
-                    ->count();
-            $count_balance = 20 - $count_accept;
             $user_ = \App\User::where('id', $leave->status_changed_by)->get();
             $st_by = '';
             if (isset($user_[0])) {
                 $st_by = $user_[0]->name;
             }
+            $unpaid_leaves = Application::where('user_id', $leave->user_id)->where('unpaid', true)->count();
             return view('applications.view')->with('data', $leave)->with('status_changed_by', $st_by)
-                            ->with('used', $count_accept)
-                            ->with('balance', $count_balance);
+                ->with('used', $leave->user->used_leave)
+                ->with('balance', $leave->user->balance_leave)
+                ->with('unpaid', $unpaid_leaves)
+                ->with('allowed', $leave->user->allowed_leave);
         } else {
             return redirect()->route('access-denied');
         }
@@ -123,23 +129,31 @@ class ApplicationsController extends Controller {
     //     $get_password = if(Auth::user()->hasPermission())
     // }
 
-    public function accept($id) {
+    public function accept($id)
+    {
         if (Auth::user()->hasPermission('accept_application')) {
             $leave = Application::where('id', $id)
-                    ->update(['status' => 1, 'status_changed_by' => Auth::user()->id]);
+                ->update(['status' => 1, 'status_changed_by' => Auth::user()->id]);
+            $apld_leave = Application::where('id', $id)->first();
+            if (!$apld_leave->unpaid) {
+                $user_ = User::where('id', $apld_leave->user_id)->get()->first();
+                $user_->balance_leave = $user_->balance_leave - $apld_leave->no_of_days;
+                $user_->used_leave = $user_->used_leave + $apld_leave->no_of_days;
+                $user_->save();
+            }
             $mail_from = $_ENV['MAIL_FROM_ADDRESS'];
             $app = Application::where('id', $id)->get();
             $mail_from_name = $_ENV['MAIL_FROM_NAME'];
-//                Notification::send($app, new LeaveApplied($app, Auth::user()));
+            //                Notification::send($app, new LeaveApplied($app, Auth::user()));
             $accepted_by = \App\User::find(Auth::user()->id);
             $applied_user = \App\User::find($app[0]->user_id);
-// Send Email to Team Lead from Employee
+            // Send Email to Team Lead from Employee
             $to_name = $app[0]->user->name;
             $to_email = $app[0]->user->email;
             $data = array('accepted_by' => $accepted_by, 'applied_user' => $applied_user, 'app_id' => $app[0]->id);
-            \Illuminate\Support\Facades\Mail:: send('mail.accept-reject-leave', $data, function($message) use ($to_name, $to_email, $mail_from, $mail_from_name) {
+            \Illuminate\Support\Facades\Mail::send('mail.accept-reject-leave', $data, function ($message) use ($to_name, $to_email, $mail_from, $mail_from_name) {
                 $message->to($to_email, $to_name)
-                        ->subject('Leave Accepted');
+                    ->subject('Leave Accepted');
                 $message->from($mail_from, $mail_from_name);
             });
             $notification = array(
@@ -158,22 +172,23 @@ class ApplicationsController extends Controller {
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function reject($id) {
+    public function reject($id)
+    {
         if (Auth::user()->hasPermission('reject_application')) {
             $leave = Application::where('id', $id)->update(['status' => 0, 'status_changed_by' => Auth::user()->id]);
             $mail_from = $_ENV['MAIL_FROM_ADDRESS'];
             $app = Application::where('id', $id)->get();
             $mail_from_name = $_ENV['MAIL_FROM_NAME'];
-//                Notification::send($app, new LeaveApplied($app, Auth::user()));
+            //                Notification::send($app, new LeaveApplied($app, Auth::user()));
             $rejected_by = \App\User::find(Auth::user()->id);
             $applied_user = \App\User::find($app[0]->user_id);
-// Send Email to Team Lead from Employee
+            // Send Email to Team Lead from Employee
             $to_name = $app[0]->user->name;
             $to_email = $app[0]->user->email;
             $data = array('rejected_by' => $rejected_by, 'applied_user' => $applied_user, 'app_id' => $app[0]->id);
-            \Illuminate\Support\Facades\Mail:: send('mail.accept-reject-leave', $data, function($message) use ($to_name, $to_email, $mail_from, $mail_from_name) {
+            \Illuminate\Support\Facades\Mail::send('mail.accept-reject-leave', $data, function ($message) use ($to_name, $to_email, $mail_from, $mail_from_name) {
                 $message->to($to_email, $to_name)
-                        ->subject('Leave Rejected');
+                    ->subject('Leave Rejected');
                 $message->from($mail_from, $mail_from_name);
             });
             $notification = array(
@@ -185,5 +200,4 @@ class ApplicationsController extends Controller {
             return redirect()->route('access-denied');
         }
     }
-
 }
--- app/Http/Controllers/LeavesController.php
+++ app/Http/Controllers/LeavesController.php
@@ -12,14 +12,16 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use App\Notifications\LeaveApplied;
 
-class LeavesController extends Controller {
+class LeavesController extends Controller
+{
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->middleware('auth');
     }
 
@@ -28,8 +30,9 @@ class LeavesController extends Controller {
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function allLeaves() {
-        $leaves = Application::where('user_id', Auth ::user()->id)->latest()->get();
+    public function allLeaves()
+    {
+        $leaves = Application::where('user_id', Auth::user()->id)->latest()->get();
         return view('leaves.all')->with('data', $leaves);
     }
 
@@ -41,43 +44,46 @@ class LeavesController extends Controller {
      * 
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function pendingLeaves() {
-        $pending_leaves = Application::where('status', 2)->where('user_id', Auth ::user()->id)
-                ->latest()
-                ->get();
+    public function pendingLeaves()
+    {
+        $pending_leaves = Application::where('status', 2)->where('user_id', Auth::user()->id)
+            ->latest()
+            ->get();
         return view('leaves.pending')->with('data', $pending_leaves);
     }
 
-    public function rejectedLeaves() {
+    public function rejectedLeaves()
+    {
         $leaves = Application::where('status', 0)
-                ->where('user_id', Auth ::user()->id)
-                ->latest()
-                ->get();
+            ->where('user_id', Auth::user()->id)
+            ->latest()
+            ->get();
         return view('leaves.rejected')->with('data', $leaves);
     }
 
-    public function acceptedLeaves() {
+    public function acceptedLeaves()
+    {
         $leaves = Application::where('status', 1)
-                ->where('user_id', Auth ::user()->id)
-                ->latest()
-                ->get();
+            ->where('user_id', Auth::user()->id)
+            ->latest()
+            ->get();
         return view('leaves.accepted')->with('data', $leaves);
     }
 
-    public function viewLeave($id) {
+    public function viewLeave($id)
+    {
         $leave = Application::find($id);
-        $count_accept = Application::where('user_id', $leave->user_id)
-                ->where('status', 1)
-                ->count();
-        $count_balance = 20 - $count_accept;
-        $user_ = \App\User::where('id', $leave->status_changed_by)->get();
-        $st_by = '';
-        if (isset($user_[0])) {
-            $st_by = $user_[0]->name;
-        }
-        return view('leaves.view')->with('data', $leave)->with('status_changed_by', $st_by)
-                        ->with('used', $count_accept)
-                        ->with('balance', $count_balance);
+            $user_ = \App\User::where('id', $leave->status_changed_by)->get();
+            $st_by = '';
+            if (isset($user_[0])) {
+                $st_by = $user_[0]->name;
+            }
+            $unpaid_leaves = Application::where('user_id', $leave->user_id)->where('unpaid', true)->count();
+            return view('applications.view')->with('data', $leave)->with('status_changed_by', $st_by)
+                ->with('used', $leave->user->used_leave)
+                ->with('balance', $leave->user->balance_leave)
+                ->with('unpaid', $unpaid_leaves)
+                ->with('allowed', $leave->user->allowed_leave);
     }
 
     /**
@@ -85,7 +91,8 @@ class LeavesController extends Controller {
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function apply() {
+    public function apply()
+    {
         $leavetypes = LeaveType::latest()->get();
         return view('leaves.apply')->with('leavetypes', $leavetypes);
     }
@@ -95,48 +102,35 @@ class LeavesController extends Controller {
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function add(Request $request) {
+    public function add(Request $request)
+    {
 
         $request->validate([
-            'leave_type_id'=>'required',
-            'subject'=>'required',
-            'description'=>'required',
-            'duration'=>'required',
-            'no_of_days'=>'required'
-            ]);
-
-        $user_id = Auth::user()->id;
-            
-        $leave = Application::where('user_id',$user_id)->get();
-        $leave = $leave[0];
-        $user_leave = $leave->user;
-        $st_lq = $user_leave->start_lq;
-        $exp_lq = $user_leave->lq_exp;
-
-        // echo '<pre>';
-        // print_r($user_leave);
-        // die;
-        $count_accept = Application::where('user_id', $leave->user_id)
-            ->where('status', 1)
-            ->count();
-
-            $count_balance = 20 - $count_accept;
-            //return $count_accept;
-            if($count_accept >= 20)
-                {
-                    $notification = array(
-                        'message' => 'You have complete the total allowed leaves.',
-                        'alert-type' => 'info'
-                    );
-                    return redirect()->route('leave-apply')->with($notification);
-                }
-            else
-            {
+            'leave_type_id' => 'required',
+            'subject' => 'required',
+            'description' => 'required',
+            'duration' => 'required',
+            'no_of_days' => 'required'
+        ]);
+        $data = $request->input();
+        $user_ = Auth::user();
+        $unpaid = false;
+        if ($user_->balance_leave > 0) {
+            if ($data['no_of_days'] > $user_->balance_leave) {
+                $notification = array(
+                    'message' => 'You have ' . $user_->balance_leave . ' Leaves only',
+                    'alert-type' => 'warning'
+                );
+                return redirect()->route('leave-apply')->with($notification);
+            }
+        } else {
+            $unpaid = true;
+        }
         date_default_timezone_set("Asia/Karachi");
         $date = date('d-m-Y h:i:s A');
         $date_new = date('d-m-Y');
 
-        $data = $request->input();
+
         $dates = explode(' - ', $data['duration']);
         $islate = $this->isLate($dates[0], $data['no_of_days']);
         try {
@@ -151,15 +145,10 @@ class LeavesController extends Controller {
             $app->team_lead = Auth::user()->team_lead;
             $app->late_apply = $islate;
             $app->datetime = $date;
+            $app->unpaid = $unpaid;
             $app->half = isset($data['short_leave']) && $data['short_leave'] == true ? $data['half'] : 0;
             $app->short_leave = isset($data['short_leave']) && $data['short_leave'] == true ? true : false;
-            $lq_exp_date = \App\User::find(Auth::user()->lq_exp);
-            $lq_start_date = \App\User::find(Auth::user()->start_lq);
-            
-            $lq_start = strtotime($lq_start_date);
-            $lq_exp = strtotime($lq_exp_date);
-            $current_date = strtotime($date_new);
-               
+
             if ($app->save()) {
                 $mail_from = $_ENV['MAIL_FROM_ADDRESS'];
                 $mail_from_name = $_ENV['MAIL_FROM_NAME'];
@@ -170,13 +159,13 @@ class LeavesController extends Controller {
                 $to_name = $team_lead->name;
                 $to_email = $team_lead->email;
                 $data = array('currentuser' => $currentuser, 'team_lead' => $team_lead, 'app_id' => $app->id);
-                \Illuminate\Support\Facades\Mail:: send('mail.apply-leave', $data, function($message) use ($to_name, $to_email, $currentuser, $mail_from, $mail_from_name) {
+                \Illuminate\Support\Facades\Mail::send('mail.apply-leave', $data, function ($message) use ($to_name, $to_email, $currentuser, $mail_from, $mail_from_name) {
                     $message->to($to_email, $to_name)
-                            ->subject('Leave Apply');
+                        ->subject('Leave Apply');
                     $message->from($mail_from, $mail_from_name);
                 });
             }
-        
+
             $notification = array(
                 'message' => 'Your Application has been submitted successfully!',
                 'alert-type' => 'success'
@@ -189,11 +178,10 @@ class LeavesController extends Controller {
             );
             return redirect()->route('leave-apply')->with($notification);
         }
-        }
-        
     }
 
-    function isLate($start_date, $days) {
+    function isLate($start_date, $days)
+    {
         $st_date = strtotime($start_date);
         $datediff = ceil(($st_date - time()) / 60 / 60 / 24);
         $days = (float) $days;
@@ -216,5 +204,4 @@ class LeavesController extends Controller {
         }
         return false;
     }
-
 }
